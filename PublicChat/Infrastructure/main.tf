025e3df3c144c2b6f27e8cff90cdd3eb6aba929e
Vagabundo <robertodelrior@gmail.com>
2021-09-28 10:47:07 +0100

    Add docker and terraform

---
+++ PublicChat/Infrastructure/main.tf
@@ -0,0 +1,72 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+terraform {
+  backend "remote" {
+    organization = "Vagacorp"
+
+    workspaces {
+      name = "web-chat"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    Environment = "Web chat"
+    Team        = "VagaDevOps"
+  }
+}
+
+resource "azurerm_app_service_plan" "freeplan" {
+  name                = "${var.resource_group_name}-plan"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_container_group" "webchat_containergroup" {
+  name                = "${var.resource_group_name}-containergroup"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_address_type = "public"
+  dns_name_label  = "vagabundo-${var.project_name}"
+  os_type         = "Linux"
+
+  container {
+    name   = var.project_name
+    image  = "vagabundocker/${var.project_name}:latest"
+    cpu    = "1"
+    memory = "1"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  tags = {
+    Environment = "Web chat"
+  }
+}
---
+++ PublicChat/Infrastructure/variables.tf
@@ -0,0 +1,11 @@
+variable "project_name" {
+  default = "webchat"
+}
+
+variable "location" {
+  default = "westeurope"
+}
+
+variable "resource_group_name" {
+  default = "webchat-dev"
+}
